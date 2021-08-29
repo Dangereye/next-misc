4d139d95596fdf78b2aab4e788f7dbb3122e34ab
Craig Puxty <craig_puxty@talktalk.net>
2021-08-29 20:22:42 +0100

    Enviroment varaibles

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -10,6 +10,7 @@ function Blog({ title, description }) {
       <main className="main">
         <div className="container">
           <h1>Blog Page</h1>
+          <p>Public variable: {process.env.NEXT_PUBLIC_PUBLIC_VARIABLE}</p>
         </div>
       </main>
     </>
@@ -19,6 +20,11 @@ function Blog({ title, description }) {
 export default Blog;
 
 export async function getServerSideProps() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+  console.log(
+    `Connecting to database with username ${user} and password ${password}`
+  );
   return {
     props: {
       title: "Article Title",
